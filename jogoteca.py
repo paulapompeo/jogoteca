02d63fdf5cd496da50e2408a270355e8253a51c2
PaulaPompeodeCamargo <paulapompeoc@gmail.com>
2020-02-18 15:32:38 -0300

    login completo

--- jogoteca.py
+++ jogoteca.py
@@ -10,6 +10,17 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+class Usuario:
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
+
+usuario1 = Usuario('paula', 'Paula Pompeo', '123456')
+usuario2 = Usuario('bino', 'Bino Carneiro', 'passear')
+
+usuarios = {usuario1.id: usuario1,
+            usuario2.id: usuario2}
 
 jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
 jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
@@ -47,13 +58,15 @@ def login():
 
 @app.route('/autenticar', methods=['POST', ])
 def autenticar():
-    if 'mestra' == request.form['senha']:
-        session['usuario_logado'] = request.form['usuario']
-        flash(request.form['usuario'] + ' logou com sucesso!')
-        proxima_pagina = request.form['proxima']
-        return redirect(proxima_pagina)
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if usuario.senha == request.form['senha']:
+            session['usuario_logado'] = usuario.id
+            flash(usuario.nome + ' logou com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
     else:
-        flash('Não logado, tente novamente')
+        flash('Não logado, tente novamente!')
         return redirect(url_for('login'))
 
 @app.route('/logout')
