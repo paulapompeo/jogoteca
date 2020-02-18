b07551f553bf2ce1c79baff81fb10ea71c9f9b29
PaulaPompeodeCamargo <paulapompeoc@gmail.com>
2020-02-18 14:37:12 -0300

    modificaçao

--- jogoteca.py
+++ jogoteca.py
@@ -23,6 +23,8 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login?proximo=novo')
     return render_template('novo.html', titulo='Novo Jogo')
 
 
@@ -39,7 +41,8 @@ def criar():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
 
 @app.route('/autenticar', methods=['POST', ])
@@ -47,7 +50,8 @@ def autenticar():
     if 'mestra' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
         flash(request.form['usuario'] + ' logou com sucesso!')
-        return redirect('/')
+        proxima_pagina = request.form['proxima']
+        return redirect('/{}'.format(proxima_pagina))
     else:
         flash('Não logado, tente novamente')
         return redirect('/login')
